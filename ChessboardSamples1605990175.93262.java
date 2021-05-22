27f9afe5dbbe387bab57dfa2a8acf749945c6f03
Topstack-first <topstack2020+first@gmail.com>
2021-05-23 05:12:58 +0900

    upload

---
+++ ChessboardSamples1605990175.93262.java
@@ -0,0 +1,54 @@
+
+/**
+ * Examples of chessboards for testing
+ */
+public final class ChessboardSamples {
+
+	public static final String sChessboard1 = 
+			" ####\n" +
+			"  ###\n" +
+			"#   #\n" +
+			"## G#\n" +
+			"#####\n";
+
+	public static final String sChessboard2 = 
+			" ###\n" +
+			"   #\n" +
+			"# G#\n" +
+			"####\n";
+
+	public static final String sChessboard3 = 
+					"  #############\n" +
+					"#  #      #   #\n" +
+					"#  #      #####\n" +
+					"#  ########   #\n" +
+					"#             #\n" +
+					"######### #####\n" +
+					"#       #     G ̰\n" +
+					"###############\n";
+
+	public static final String sChessboard4 = 
+			" ####\n" +
+			"    #\n" +
+			"#   #\n" +
+			"#  G#\n" +
+			"#####\n";
+
+	public static final String sChessboard5 = 
+					"  ########\n" +
+					"#  ###   #\n" +
+					"#        #\n" +
+					"#  ##### #\n" +
+					"#        G\n" +
+					"###########\n";
+
+	public static final String sChessboard6 = 
+					" #######\n" +
+					"       #\n" +
+					"#     G#\n" +
+					"########\n";
+
+    
+}
+
+
